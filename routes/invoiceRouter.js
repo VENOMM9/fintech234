14493553db751d2acbf52909468f183a7429569b
Victor Nkwuo <vobinn3@gmail.com>
2023-11-25 13:48:06 +0300

    recloned fintech repo

--- routes/invoiceRouter.js
+++ routes/invoiceRouter.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const controller = require("../controllers/controller")
+// const express = require("express");
+// const controller = require("../controllers/controller")
 
 
 
-const invoiceRouter = express.Router();
+// const invoiceRouter = express.Router();
 
-module.exports = invoiceRouter
+// module.exports = invoiceRouter
 
 
